d111e69a9ea8be2542336a12ee432295e3cbfac9
Oluwa-seun <oluwaseunolubiyione@gmail.com>
2022-09-12 11:47:55 +0100

    done

---
+++ src/content_option.js
@@ -0,0 +1,197 @@
+const logotext = "OS";
+const meta = {
+    title: "Olubiyi Oluwaseun",
+    description: "Olubiyi's portfolio.",
+};
+
+const introdata = {
+    title: "Oluwaseun Olubiyi",
+    animated: {
+        first: "Frontend Developer",
+        second: "UI/UX Designer",
+        third: "& sprinkle of Backend",
+    },
+    description: "ye'llo world, i'm seun a computer science major writting and pushing code from the metavers. ",
+    your_img_url: ("../.."),
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme:"Over the past 02 years, i've built products for companies and individuals around africa ranging from marketing website to complex solutions and enterprise apps with focus on fast, elegant and accessible user experience.",
+    aboutme2:"Curently, i work at spectrangle as a Junior Developer crafting thoughtful and inclusive experience that adher to all web standards.",
+    aboutme3:"Before now, interned at colab where i worked on coverting Figma UIs to interactive codes for orgs and individuals. Prior to colab, i was building product for individuals around the world on fiver and upwork as a freelance developer."
+}
+const worktimeline = [{
+        jobtitle: "Frontend Developer",
+        where: "Spectrangle",
+        date: "Present",
+    },
+    {
+        jobtitle: "Intern",
+        where: "Colab",
+        date: "2021",
+    },
+    {
+        jobtitle: "Intern of the year",
+        where: "NIIT",
+        date: "2020",
+    },
+    {
+        jobtitle: "Enginer",
+        where: "OilTech Limited",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "Javascript",
+        value: 60,
+    },
+    {
+        name: "Typescript",
+        value: 40,
+    },
+    {
+        name: "Vuejs",
+        value: 70,
+    },
+    {
+        name: "Reactjs",
+        value: 50,
+    },
+    {
+        name: "Firebase",
+        value: 80,
+    },
+    {
+        name: "git",
+        value: 85,
+    },
+    {
+        name: "bootstrap",
+        value: 85,
+    },
+
+    
+];
+
+const services = [{
+        title: "Web Development",
+        description: "I provide everything you need for your website",
+    },
+    {
+        title: "Landing Pge",
+        description: "I build amazing landing pages for bussiness owners that matches their Brand",
+    },
+    {
+        title: "Collaboration",
+        description: "I'm available for collaboration and contribution to amazing project ideas",
+    },
+    {
+        title: "Tailored Development",
+        description: "I build with your goals in mind, while keeping code maintainable for easy update.",
+    },
+    {
+        title: "Contract",
+        description: "I offer broad spectrum of web development services to fully tap into the capabilities offered by modern technologies",
+    },
+    
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "oluwaseunolubiyione@gmail.com",
+    YOUR_FONE: "+2348154926454",
+    description: "Do you need help building the next facebook or flutterwave ",
+    // creat an emailjs.com account when you are ready for production
+    // connecting the backend of the email to the frontend of this application 
+    // to recive email from users
+    // note to future self!...  
+    // incase your memory preceeds you use next line 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    // juju was here..
+    YOUR_SERVICE_ID: "gmail",
+    YOUR_TEMPLATE_ID: "template_cr6koem",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    linkedin: "https://linkedin.com",
+    behance: "https://behance.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
