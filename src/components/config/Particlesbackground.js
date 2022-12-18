9314bad5fa4aa4b1d7116df1512bd9581bdf5bf7
Oluwa-Seun <oluwaseunolubiyione@gmail.com>
2022-12-18 10:19:13 +0100

    fixed config

---
+++ src/components/config/Particlesbackground.js
@@ -0,0 +1,14 @@
+import Particles from "react-tsparticles";
+import ParticlesConfig from './ParticlesConfig';
+
+
+
+function Particlesbackground () {
+  
+  return (
+    
+          <Particles params={ParticlesConfig} height="100vh" width="100vw" />
+  );
+}
+
+export default Particlesbackground;
